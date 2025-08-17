3ea8b73b23cfd23ec0d511f8033a246ed4199233
AbdulazizErgashev <abdulazizergashev0335@gmail.com>
2025-08-17 18:41:21 +0500

    updated routes

--- routes/mathRoutes.js
+++ routes/mathRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { solveMathProblem } from "../controllers/mathController.js";
+import {
+  solveMathProblem,
+  getMathHelp,
+} from "../controllers/mathController.js";
 
 const router = express.Router();
 
 router.post("/solve", solveMathProblem);
+router.get("/help", getMathHelp);
 
 export default router;
